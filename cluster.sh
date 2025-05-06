347c4568f4bc97d10595b8b03932c4215c8a4675
BraCR10 <brianramirez01arias@gmail.com>
2025-05-06 10:28:08 -0600

    Refactoring call

--- cluster.sh
+++ cluster.sh
@@ -7,7 +7,7 @@ export NETWORK="simnet"
 docker volume create simnet_lnd_a
 docker volume create simnet_lnd_b
 
-cd LND/docker
+cd lnd/docker
 docker-compose  run -d --name node_a --volume simnet_lnd_a:/root/.lnd lnd
 
 docker logs node_a
